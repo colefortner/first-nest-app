977e1e6284fa0c0a6904c14db50d11e9f56add0e
Cole Fortner <colefortner@gmail.com>
2022-11-02 20:37:48 -0400

    Generate  user and business modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BusinessModule } from './business/business.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, BusinessModule],
 })
 export class AppModule {}
